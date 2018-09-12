42bc68c125c2abe83b4b2f5f6f6e726cdd2fd294
Jeremy lumley <Jeremy.Lumley@carleton.ca>
2018-09-12 17:33:17 -0400

    adding test cases

--- src/test/java/blackjack/DeckTest.java
+++ src/test/java/blackjack/DeckTest.java
@@ -9,6 +9,20 @@ public class DeckTest extends TestCase {
 		deck.shuffle_deck();
 		deck.print_deck();
 		
+		
+	}
+	
+	public void testDeal() {
+		Deck deck = new Deck();
+		Player p1 = new Player(false);
+		Player p2 = new Player(true);
+		
+		deck.deal(p1, p2);
+		
+		assertEquals(p1.hand[0].suit, "S");
+		assertEquals(p2.hand[0].suit, "S");
+		assertEquals(p1.hand[0].name, "A");
+		assertEquals(p1.hand[1].name, "3");
 	}
 
 }
