716337c0fda8d689f15e81b0d794daec147caa80
Jeremy lumley <Jeremy.Lumley@carleton.ca>
2018-09-19 13:18:55 -0400

    adding methods for player class

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -1,19 +1,30 @@
 package blackjack;
 
+import java.util.Scanner;
+
 import blackjack.Card; 
 
 public class Player {
 	
 	boolean isDealer;
+	String name;
 	Card[] hand;
+	int numberOfCards = 0;
 	int points;
 	boolean didWin;
-	
+	boolean hasAce = false;
 	
 
 	public Player(boolean isDealer) {
 		// TODO Auto-generated constructor stub
 		this.isDealer = isDealer;
+		if (isDealer) {
+			this.name = "Dealer";
+		} else {
+			this.name = "Player";
+		}
+		this.hand = new Card [10];
+		this.didWin = false;
 		
 	}
 	
@@ -32,8 +43,18 @@ public class Player {
 				} else if (card.value == 11) {
 					card.setValue(1);
 					this.calculatePoints();
+					break;
 				}
 			}
+		}
+	}
+	
+	public boolean blackjackCheck() {
+		this.calculatePoints();
+		if (this.points == 21 ) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
@@ -43,9 +64,72 @@ public class Player {
 			return true;
 		} else if (this.points < opponent.points || this.points > 21 ) {
 			return false;
-		} else {
+		} else if (this.points > opponent.points){
 			return true;
+		} else {
+			return this.isDealer;
 		}
+	}
+	
+	public void print_cards(boolean printPoints) {
+		System.out.println(this.name + ": ");
+		for (Card card: hand) {
+			if (card == null) {
+				continue;
+			}
+			if (card.hidden) {
+				System.out.print("XX ");
+			} else {
+			    System.out.print(card.suit + card.name + " ");
+			}
+		}
+		if (printPoints) {
+			this.calculatePoints();
+			System.out.print(" -> " + Integer.toString(this.points));
+		}
+		System.out.println();
+	}
+	
+	public boolean decision(Scanner scanner) {
+		boolean hit = false;
+		this.calculatePoints();
+
+		if (!this.isDealer ) {
+			System.out.println("Hit (H) or Stand (S)");
+			while (true) {
+				String choice = scanner.next();
+				if (choice == "") {
+					continue;
+				}
+				if (choice.toUpperCase().charAt(0) == 'H') {
+					hit = true;
+					break;
+				} else if ( choice.toUpperCase().charAt(0) == 'S') {
+					hit = false;
+					break;
+				} else {
+					System.out.println("Invalid option given.");
+				}
+			}
+			
+		} else {
+			// hit on a soft 17
+			if (this.points == 17 && this.hasAce || this.points < 17) {
+				hit = true;
+			} else {
+				hit = false;
+			}
+			
+		}
+		return hit;
+	}
+	
+	public void addCard(Card card) {
+		if (card.name == "A") {
+			this.hasAce = true;
+		}
+		this.hand[numberOfCards] = card;
+		this.numberOfCards += 1;
 	}
 	
 	public void setPoints(int points) {
@@ -56,3 +140,5 @@ public class Player {
 		this.hand = hand;
 	}
 }
+
+
